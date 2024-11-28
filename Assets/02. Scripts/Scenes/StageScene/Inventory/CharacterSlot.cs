e542c94583b5cd9f0fb36a1d5813a48fb817fdbc
udiladido <89720389+udiladido@users.noreply.github.com>
2024-11-28 20:48:24 +0900

    Add : 인벤토리(캐릭터) 작업

---
+++ Assets/02. Scripts/Scenes/StageScene/Inventory/CharacterSlot.cs
@@ -0,0 +1,50 @@
+
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class CharacterSlot : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler, IDropHandler,
+    IPointerDownHandler, IPointerUpHandler,
+    IPointerEnterHandler, IPointerExitHandler
+{
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+       
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        
+    }
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+       
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+       
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+       
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        
+    }
+
+ 
+}
---
+++ Assets/02. Scripts/Scenes/StageScene/Inventory/Inventory.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+
+
+    private InventoryCharacter[] _inventoryCharacter = new InventoryCharacter[Defines.CHARACTER_INVENTORY_SIZE];
+
+
+    public void SwapItem()
+    {
+        // 인벤토리에서 꺼낸 캐릭터와 필드 위의 캐릭터를 서로 바꾸어야 함.
+       
+
+    }
+
+
+}
