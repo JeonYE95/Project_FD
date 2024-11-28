528ec541773cd4358bd9e24725f597e270faaa81
moomin <amily8050@gmail.com>
2024-11-28 17:09:51 +0900

    UPDATE : UIMain 동적 생성 틀 구현

--- Assets/02. Scripts/UI/UIExit.cs
+++ Assets/02. Scripts/UI/UIExit.cs
@@ -1,6 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
-public class UIMenu : UIBase
+public class UIExit : UIBase
 {
     // Start is called before the first frame update
     void Start()
--- Assets/02. Scripts/UI/UIMain.cs
+++ Assets/02. Scripts/UI/UIMain.cs
@@ -4,19 +4,41 @@ using UnityEngine.UI;
 public class UIMain : UIBase
 {
     [SerializeField] private Button startBtn;
+    [SerializeField] private Button settingBtn;
+    [SerializeField] private Button exitBtn;
  
     private UISelectStage uiSelectStage;
-    
+    private UISetting uiSetting;
+    private UIExit uiExit;
+
     private void Start()
     {
-        startBtn.onClick.AddListener(() => { OpenSelectStage(); });
+        startBtn.onClick.AddListener(() => { OpenSelectStageUI(); });
+        settingBtn.onClick.AddListener(() => { OpenSettingUI(); });
+        exitBtn.onClick.AddListener(() => { OpenExitUI(); });
     }
 
-    private void OpenSelectStage()
+    private void OpenSelectStageUI()
     {
         if (uiSelectStage == null)
             uiSelectStage = UIManager.Instance.GetUI<UISelectStage>();
         
         uiSelectStage.Open();
+    }
+
+    private void OpenSettingUI()
+    {
+        if (uiSetting == null)
+            uiSetting = UIManager.Instance.GetUI<UISetting>();
+        
+        uiSetting.Open();
+    }
+
+    private void OpenExitUI()
+    {
+        if (uiExit == null)
+            uiExit = UIManager.Instance.GetUI<UIExit>();
+        
+        uiExit.Open();
     }
 }
