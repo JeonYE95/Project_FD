9366d590678251f3b7215adc881de73b203e70be
udiladido <89720389+udiladido@users.noreply.github.com>
2024-11-28 18:02:25 +0900

    Update : 스크립트 이름 변경
    StageSpawn.cs -> WaveSpawn.cs

--- Assets/02. Scripts/Scenes/StageScene/WaveSpawn.cs
+++ Assets/02. Scripts/Scenes/StageScene/WaveSpawn.cs
@@ -3,12 +3,10 @@ using System.Threading;
 using UnityEngine;
 using UnityEngine.TextCore.Text;
 
-public class StageSpawn : MonoBehaviour
+public class WaveSpawn : MonoBehaviour
 {
-  
 
     //나중에 SpawnPoint와 연결
-
 
     public int monsterSpawnCount { get; private set; }
     public int playerSpawnCount { get; private set; }
@@ -16,20 +14,15 @@ public class StageSpawn : MonoBehaviour
 
     private void SpawnEntity(int pointGroup, int point, int ID)
     {
-       
-   
 
         //몬스터,캐릭터에서 죽었을 때 이벤트에 Dead 추가 
 
-
     }
-
 
     // 상속 등으로 분리 해 각각에서 처리 - 캐릭터, 몬스터
     /*
       private void Dead(int identifier)
     {
-
         monsterSpawnCount--;
    
         if ((monsterSpawnCount == 0))
@@ -38,7 +31,5 @@ public class StageSpawn : MonoBehaviour
         }
     }
      */
-
-
 
 }
