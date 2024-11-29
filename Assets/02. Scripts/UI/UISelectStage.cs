5f18d36ed714606dce835e8bea4299e2303a1b51
moomin <amily8050@gmail.com>
2024-11-29 17:58:35 +0900

    UPDATE : UIInGame 프리팹 수정
    인게임 내부 UI 틀 구현(스크롤뷰, 버튼)
    UISelectStage에 Exit버튼 추가

--- Assets/02. Scripts/UI/UISelectStage.cs
+++ Assets/02. Scripts/UI/UISelectStage.cs
@@ -8,12 +8,16 @@ public class UISelectStage : UIBase
     [SerializeField] private Button stageBtn1_3;
     [SerializeField] private Button stageBtn1_4;
     [SerializeField] private Button stageBtn1_5;
+    [SerializeField] private Button exitBtn;
 
     private UIInGame uiInGame;
+    private UISelectStage uISelectStage;
 
     void Start()
     {
         stageBtn1_1.onClick.AddListener(() => { OpenInGameUI(1); });
+        
+        exitBtn.onClick.AddListener(() => { Close(); });
     }
 
     private void OpenInGameUI(int stageNum)
