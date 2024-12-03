26f7ffb65a4413b521baac88d27adb2058f3c025
JeonYE95 <eunzse@gmail.com>
2024-12-03 20:26:31 +0900

    Update : kc
    에러 주석처리

--- Assets/02. Scripts/Scenes/MainScene/OutStageShop.cs
+++ Assets/02. Scripts/Scenes/MainScene/OutStageShop.cs
@@ -2,32 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class OutStageShop : ShopBase
-{
+//public class OutStageShop : ShopBase
+//{
 
-    private DataManager _dataManager => DataManager.Instance;
+//    private DataManager _dataManager => DataManager.Instance;
 
-    protected override void AddGold(int value)
-    {
+//    protected override void AddGold(int value)
+//    {
 
-        _dataManager.PlayerData.Gold += value;
-    
-    }
+//        _dataManager.PlayerData.Gold += value;
 
-    protected override bool HasGold(int value)
-    {
+//    }
 
-        return _dataManager.PlayerData.Gold >= value;
+//    protected override bool HasGold(int value)
+//    {
 
-    }
+//        return _dataManager.PlayerData.Gold >= value;
 
-    protected override void UseGold(int value)
-    {
+//    }
 
-        _dataManager.PlayerData.Gold -= value;
+//    protected override void UseGold(int value)
+//    {
 
-    }
+//        _dataManager.PlayerData.Gold -= value;
+
+//    }
 
 
 
-}
+//}
