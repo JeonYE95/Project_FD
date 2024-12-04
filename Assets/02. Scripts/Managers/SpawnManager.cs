a5b394d9edebd4d53d243e9a5ed2e5f540382712
udiladido <89720389+udiladido@users.noreply.github.com>
2024-12-04 20:00:28 +0900

    Update : 소환 관련 기능 및 테스트

--- Assets/02. Scripts/Managers/SpawnManager.cs
+++ Assets/02. Scripts/Managers/SpawnManager.cs
@@ -2,8 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpawnManager : MonoBehaviour
+public class SpawnManager : Singleton<SpawnManager>
 {
+
+
+    public void SpawnCharacter(Unit unit)
+    {
+
+        Instantiate(Resources.Load<GameObject>($"Prefabs/Unit/{unit.Grade}/{unit.Name}"));
+
+
+    }
+
+    public void SpawnEnemy(Unit unit)
+    {
+
+        Instantiate(Resources.Load<GameObject>($"Prefabs/Enemy/{unit.Name}"));
+
+    }
 
 
 
--- Assets/02. Scripts/UI/UIUnitSlotTest.cs
+++ Assets/02. Scripts/UI/UIUnitSlotTest.cs
@@ -5,8 +5,7 @@ using UnityEngine.UI;
 
 public class UIUnitSlotTest : MonoBehaviour
 {
-    // [SerializeField] private Image unitImg;
-    // [SerializeField] private TMP_Text unitNameTxt;
+   
     private RectTransform content;
 
     [SerializeField] private GameObject unitSlotPrefab;
@@ -25,72 +24,20 @@ public class UIUnitSlotTest : MonoBehaviour
 
         // 테스트 코드
         Unit unit1 = gameObject.AddComponent<Unit>();
-        unit1.UnitName = "nametest1";
-        unit1.SpritePath = "Unit1";
+        unit1.Name = "Archer";
+        unit1.Grade = "Common";
         inventoryUnits.Add(unit1);
 
         Unit unit2 = gameObject.AddComponent<Unit>();
-        unit2.UnitName = "nametest2";
-        unit2.SpritePath = "Unit1";
+        unit2.Name = "Healer";
+        unit2.Grade = "Common";
         inventoryUnits.Add(unit2);
 
-        Unit unit3 = gameObject.AddComponent<Unit>();
-        unit3.UnitName = "nametest3";
-        unit3.SpritePath = "Unit1";
-        inventoryUnits.Add(unit3);
-
-        Unit unit4 = gameObject.AddComponent<Unit>();
-        unit4.UnitName = "nametest4";
-        unit4.SpritePath = "Unit1";
-        inventoryUnits.Add(unit4);
-
-        Unit unit5 = gameObject.AddComponent<Unit>();
-        unit5.UnitName = "nametest5";
-        unit5.SpritePath = "Unit1";
-        inventoryUnits.Add(unit5);
-
-        Unit unit6 = gameObject.AddComponent<Unit>();
-        unit6.UnitName = "nametest6";
-        unit6.SpritePath = "Unit1";
-        inventoryUnits.Add(unit6);
-
-        Unit unit7 = gameObject.AddComponent<Unit>();
-        unit7.UnitName = "nametest7";
-        unit7.SpritePath = "Unit1";
-        inventoryUnits.Add(unit7);
-
-        Unit unit8 = gameObject.AddComponent<Unit>();
-        unit8.UnitName = "nametest8";
-        unit8.SpritePath = "Unit1";
-        inventoryUnits.Add(unit8);
-
-        Unit unit9 = gameObject.AddComponent<Unit>();
-        unit9.UnitName = "nametest9";
-        unit9.SpritePath = "Unit1";
-        inventoryUnits.Add(unit9);
-
-        Unit unit10 = gameObject.AddComponent<Unit>();
-        unit10.UnitName = "nametest10";
-        unit10.SpritePath = "Unit1";
-        inventoryUnits.Add(unit10);
-
-        Unit unit11 = gameObject.AddComponent<Unit>();
-        unit11.UnitName = "nametest11";
-        unit11.SpritePath = "Unit1";
-        inventoryUnits.Add(unit11);
-
-        Unit unit12 = gameObject.AddComponent<Unit>();
-        unit12.UnitName = "nametest12";
-        unit12.SpritePath = "Unit1";
-        inventoryUnits.Add(unit12);
-
-        Unit unit13 = gameObject.AddComponent<Unit>();
-        unit13.UnitName = "nametest13";
-        unit13.SpritePath = "Unit1";
-        inventoryUnits.Add(unit13);
-
+     
         CreateUnitSlots();
     }
+
+
 
     private void CreateUnitSlots()
     {
@@ -103,7 +50,7 @@ public class UIUnitSlotTest : MonoBehaviour
 
             if (unitNameTxt != null)
             {
-                unitNameTxt.text = unit.UnitName;
+                unitNameTxt.text = unit.Name;
                 Debug.Log(unitNameTxt);
             }
             else
@@ -111,7 +58,7 @@ public class UIUnitSlotTest : MonoBehaviour
 
             if (unitImg != null)
             {
-                Sprite sprite = Resources.Load<Sprite>($"Sprite/{unit.SpritePath}");
+                Sprite sprite = Resources.Load<Sprite>($"Sprite/{unit.Name}");
                 unitImg.sprite = sprite;
                 Debug.Log(unitImg);
             }
@@ -119,4 +66,16 @@ public class UIUnitSlotTest : MonoBehaviour
                 Debug.Log("Image 컴포넌트 없음.");
         }
     }
+
+    public void updateUnits(List<Unit> units)
+    {
+        foreach (Transform child in content)
+        {
+            Destroy(child.gameObject);
+        }
+        // 새로운 유닛 목록으로 업데이트
+        inventoryUnits = units;
+        CreateUnitSlots();
+    }
+
 }
