08a9c448f2a53fc8fc42113baf9447aa9f104c99
moomin <amily8050@gmail.com>
2024-12-06 11:09:06 +0900

    UPDATE : UIInGame 수정

--- Assets/02. Scripts/UI/UIInGame.cs
+++ Assets/02. Scripts/UI/UIInGame.cs
@@ -1,4 +1,5 @@
 using System;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -6,6 +7,10 @@ public class UIInGame : UIBase
 {
     [SerializeField] private Button drawBtn;
     [SerializeField] private Button unitGuideBtn;
+    [SerializeField] private TMP_Text _timerText;
+    [SerializeField] private Button _battleStartButton;
+
+    [SerializeField] private GameObject _spawnPointUI;
 
     private UIUnitGuide uiUnitGuide;
 
@@ -13,6 +18,20 @@ public class UIInGame : UIBase
     {
         drawBtn.onClick.AddListener(() => {  });    // 버튼 클릭 시 호출 함수 필요
         unitGuideBtn.onClick.AddListener(() => { OpenUnitGuideUI(); });  
+
+        // _battleStartButton = GetComponentInChildren<Button>();
+        // _timerText = GetComponentInChildren<TMP_Text>();
+
+        //WaveManager 버튼 연동
+        _battleStartButton.onClick.AddListener(WaveManager.Instance.WaveStartNow);
+        //타이머 연동
+        WaveManager.Instance.OnPreparationTimeChanged += UpdateTimerText;
+
+        // 전투 시작시 UI 비활성화 
+        WaveManager.Instance.OnBattleStart += DisablePrepUI;
+
+        //전투 종료시 UI 활성화
+        WaveManager.Instance.OnClearWave += EnablePrepUI;
     }
 
     private void OpenUnitGuideUI()
@@ -21,5 +40,23 @@ public class UIInGame : UIBase
             uiUnitGuide = UIManager.Instance.GetUI<UIUnitGuide>();
         
         uiUnitGuide.Open();
+    }
+
+    private void UpdateTimerText(float remainingTime)
+    {
+        int seconds = Mathf.FloorToInt(remainingTime % 60);
+        _timerText.text = $"{seconds:00}";
+    }
+
+    private void DisablePrepUI()
+    {
+        _battleStartButton.gameObject.SetActive(false);
+        _spawnPointUI.gameObject.SetActive(false);
+    }
+
+    private void EnablePrepUI()
+    {
+        _battleStartButton.gameObject.SetActive(true);
+        _timerText.gameObject.SetActive(true);
     }
 }
