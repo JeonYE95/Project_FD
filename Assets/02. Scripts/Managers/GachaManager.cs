30a12ffe5815e688393459d0cd2a7ce865da0f59
JeonYE95 <eunzse@gmail.com>
2024-12-06 10:10:33 +0900

    Add : GachaManager
    인게임 가챠 시스템 적용

---
+++ Assets/02. Scripts/Managers/GachaManager.cs
@@ -0,0 +1,32 @@
+using GSDatas;
+using UnityEngine;
+using System.Collections.Generic;
+
+public class GachaManager : Singleton<GachaManager>
+{
+    public int GachaGold = 5;
+
+    public void PlayGacha()
+    {
+        if (StageManager.Instance.Gold < GachaGold)
+        {
+            return;
+        }
+
+        StageManager.Instance.Gold -= GachaGold;
+
+        List<UnitData> commonUnits = new List<UnitData>();
+
+        foreach (var unit in UnitData.GetList())
+        {
+            if (unit.grade == "Common")
+            {
+                commonUnits.Add(unit);
+            }
+        }
+
+        UnitData selectedUnit = commonUnits[Random.Range(0, commonUnits.Count)];
+
+        //InventoryManager.Instance.AddCharacter(selectedUnit, 1);
+    }
+}
