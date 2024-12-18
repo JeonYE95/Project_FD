8b4ff67e15633653ea2b14a062ac31d0944fd624
udiladido <89720389+udiladido@users.noreply.github.com>
2024-12-18 18:00:27 +0900

    Update : 아이템, 유닛 정보 업데이트 저장
    newtonsoft.json 추가

--- Assets/02. Scripts/Managers/GameManager.cs
+++ Assets/02. Scripts/Managers/GameManager.cs
@@ -14,7 +14,8 @@ public class PlayerData
     public int level;
     public int gold;
     public int diamond;
-    public List<InGameItems> items = new List<InGameItems>();
+    public Dictionary<int, int> items = new Dictionary<int, int>(); // key: 아이템 ID, value: 아이템 수
+    public Dictionary<int, int> UnitInforce = new Dictionary<int, int>(); // key : 유닛 ID, value : 강화 횟수
 }
 
 [System.Serializable]
@@ -29,18 +30,34 @@ public class InGameItems
 public class GameManager : SingletonDontDestory<GameManager>
 {
 
+    private int _EnterStageID = 101;
 
-    //임시 : 버튼 클릭 시 변동되도록 나중에 연동해야 함. 
-    public int stageID = 101;
+   public int StageID
+    {
+        get { return _EnterStageID; }
+        set { _EnterStageID = value; }
+
+    }
+
+
     public int EnterEnergy = 100;
 
     // 모든 스테이지 정보 저장
     private List<StageData> _AllStageData = new List<StageData>();
-    private List<int> _TotalStageIDCount;
+    [SerializeField]
+    private List<StageData> _TotalStageID;
+
+
+    public List<StageData> TotalStageID
+    {
+        get { return _TotalStageID; }
+        private set { _TotalStageID = value; }
+
+    }
 
 
     public static readonly int BehindSortingOrder = 199;
-    public static readonly int EnemySortingOrder  = 200;
+    public static readonly int EnemySortingOrder = 200;
     public static readonly int PlayerSortingOrder = 201;
 
     public PlayerData playerData = new PlayerData();
@@ -53,7 +70,7 @@ public class GameManager : SingletonDontDestory<GameManager>
         UIManager.Instance.Initialize();
 
         GetAllStatgeData();
-
+        StageCount();
     }
 
     //인 게임에서 변동 시 JSON 관리
@@ -62,7 +79,7 @@ public class GameManager : SingletonDontDestory<GameManager>
     void SavePlayerDataToJson()
     {
         // JSON 생성
-        string jsonData = JsonUtility.ToJson(playerData, true);
+        string jsonData = Newtonsoft.Json.JsonConvert.SerializeObject(playerData, Newtonsoft.Json.Formatting.Indented);
         // 데이터 경로 지정
         string path = Path.Combine(Application.dataPath, "playerData.json");
         // 파일 생성 및 저장
@@ -82,15 +99,14 @@ public class GameManager : SingletonDontDestory<GameManager>
             // 파일의 텍스트를 string으로 저장
             string jsonData = File.ReadAllText(path);
             // 이 Json데이터를 역직렬화하여 playerData에 넣어줌
-            playerData = JsonUtility.FromJson<PlayerData>(jsonData);
-
+            playerData = Newtonsoft.Json.JsonConvert.DeserializeObject<PlayerData>(jsonData);
 
         }
 
     }
 
-
-    public void AddItem(int itemId, int count)
+    //보상 획득 시 저장 
+    public void AddItemSave(int itemId, int count)
     {
 
         if (itemId == 3001)
@@ -101,27 +117,56 @@ public class GameManager : SingletonDontDestory<GameManager>
 
         }
 
-
         // 기존 아이템이 있는지 확인
-        var existingItem = playerData.items.Find(item => item.id == itemId);
-
-        if (existingItem != null)
+        if (playerData.items.TryGetValue(itemId, out int currentCount))
         {
-            existingItem.count += count;
+            playerData.items[itemId] = currentCount + count;
         }
         else
         {
-            var newItem = new InGameItems
-            {
-                id = itemId,
-                count = count
-            };
-            playerData.items.Add(newItem);
+            playerData.items.Add(itemId, count);
         }
 
         // 자동 저장
         //SavePlayerDataToJson();
     }
+
+    // 아이템 소비 시 저장
+    public void substractItemSave(int itemId, int count)
+    {
+
+        // 기존 아이템이 충분히 있는지 확인해서 감소 후 저장
+
+        if (playerData.items.TryGetValue(itemId, out int currentCount))
+        {
+            if (currentCount >= count)
+            {
+                int remainCount = currentCount - count;
+                if (remainCount <= 0)
+                {
+                    playerData.items.Remove(itemId);  // 수량이 0이 되면 제거
+                }
+                else
+                {
+                    playerData.items[itemId] = remainCount;
+                }
+                SavePlayerDataToJson();
+            }
+            else
+            {
+                Debug.LogWarning($"아이템 부족: 필요 수량 {count}, 보유 수량 {currentCount}");
+            }
+        }
+        else
+        {
+            Debug.LogWarning($"해당 아이템이 없습니다: {itemId}");
+        }
+
+
+        // 자동 저장
+        //SavePlayerDataToJson();
+    }
+
 
     //전체 스테이지 보상 데이터 가져오기 
     private void GetAllStatgeData()
@@ -131,7 +176,7 @@ public class GameManager : SingletonDontDestory<GameManager>
 
     }
 
-    //특정 스테이지 보상 반환 
+    //특정 스테이지 보상 목록 반환 
     public List<StageData> GetStatgeData(int stageID)
     {
 
@@ -151,15 +196,35 @@ public class GameManager : SingletonDontDestory<GameManager>
 
             Debug.Log($" 스테이지 클리어 보상 : {rewardInfo.name} : {reward.count} 획득");
 
-            AddItem(reward.RewardID, reward.count);
+            AddItemSave(reward.RewardID, reward.count);
 
         }
 
     }
 
+    //유닛 강화 정보 저장
+    public void EnforceUnitSave(int unitID)
+    {
+
+        // 기존 유닛의 강화 정보가 있는지 확인
+        if (playerData.UnitInforce.TryGetValue(unitID, out int currentEnforce))
+        {
+            playerData.UnitInforce[unitID] = currentEnforce + 1;
+        }
+        else
+        {
+            // 처음 강화하는 유닛이면 1부터 시작 - 임의로 1 넣음 추후 UGS 데이터에 맞게 숫자 조정
+            playerData.UnitInforce.Add(unitID, 1);
+        }
+
+        SavePlayerDataToJson();
+
+    }
+
+
     public void StageCount()
     {
-        _TotalStageIDCount = _AllStageData.Select(data => data.ID).Distinct().ToList();
+        _TotalStageID = _AllStageData.GroupBy(data => data.ID).Select(group => group.First()).ToList(); ;
 
     }
 
