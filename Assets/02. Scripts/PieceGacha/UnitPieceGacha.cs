7dff0c64c8ddba9e987a67b4258fb966229942d6
JeonYE95 <eunzse@gmail.com>
2024-12-19 20:36:25 +0900

    Update : Piece Gacha
    가챠피스 10연차 추가

--- Assets/02. Scripts/PieceGacha/UnitPieceGacha.cs
+++ Assets/02. Scripts/PieceGacha/UnitPieceGacha.cs
@@ -1,11 +1,12 @@
 using UnityEngine;
 using System.Collections.Generic;
 using GSDatas;
+using System;
 
 public class UnitPieceGacha : MonoBehaviour
 {
     private int _gachaCost = 200;
-    private int _diamond = 1000;
+    private int _diamond = 10000;
 
     private readonly Dictionary<string, int> _pieceAmount = new Dictionary<string, int>()
     {
@@ -25,29 +26,49 @@ public class UnitPieceGacha : MonoBehaviour
         _diamond -= _gachaCost;
         Debug.Log($"다이아를 {_gachaCost}만큼 사용했습니다. 남은 다이아 : {_diamond}");
 
-        List<UnitData> allUnits = UnitDataManager.Instance.GetUnitDatas();
-        UnitData selectedUnit = GetUnitByWeight(allUnits);
+        UnitData selectedUnit = GetUnitByWeight();
 
         if (selectedUnit != null)
         {
             int pieceAmount = GetPieceAmountByGrade(selectedUnit.grade);
             Debug.Log($"뽑힌 유닛 : {selectedUnit.name} , 등급 : {selectedUnit.grade} , 조각 개수 : {pieceAmount}");
         }
+
     }
 
-    private UnitData GetUnitByWeight(List<UnitData> units)
+    public void PlayPieceGacha10()
     {
+        int totalGachaCost = _gachaCost * 10;
+
+        if (_diamond < totalGachaCost)
+        {
+            Debug.LogWarning("다이아가 부족합니다");
+            return;
+        }
+
+        Debug.Log($"다이아를 {totalGachaCost}만큼 사용했습니다. 남은 다이아 : {_diamond}");
+
+        for (int i = 0; i < 10; i++)
+        {
+            PlayPieceGacha();
+        }
+    }
+
+    private UnitData GetUnitByWeight()
+    {
+        List<UnitData> unitList = UnitDataManager.Instance.GetUnitDatas();
         int totalWeight = 0;
 
-        foreach (var unit in units)
+        foreach (var unit in unitList)
         {
             totalWeight += unit.weight;
         }
-
-        int randomValue = Random.Range(0, totalWeight + 1);
+        
+        System.Random random = new System.Random();
+        int randomValue = random.Next(1, totalWeight + 1);
         int cumulativeValue = 0;
 
-        foreach (var unit in units)
+        foreach (var unit in unitList)
         {
             cumulativeValue += unit.weight;
             if (randomValue <= cumulativeValue)
