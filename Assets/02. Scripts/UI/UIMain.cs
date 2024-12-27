ae779514d2ba2c8ae8825faaacac2433e8ec1fce
moomin <amily8050@gmail.com>
2024-12-27 20:34:21 +0900

    UPDATE : UIMain
    버튼 및 젬 표시 UI 추가

--- Assets/02. Scripts/UI/UIMain.cs
+++ Assets/02. Scripts/UI/UIMain.cs
@@ -5,7 +5,7 @@ public class UIMain : UIBase
 {
     [SerializeField] private Button _startBtn;
     [SerializeField] private Button _settingBtn;
-    [SerializeField] private Button _exitBtn;
+    [SerializeField] private Button _unitStateBtn;
 
     private void Start()
     {
