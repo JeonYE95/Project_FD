83ddd5464f8c76e999388239feb1f38c2f429642
moomin <amily8050@gmail.com>
2024-12-29 21:03:22 +0900

    ADD : UIHeroes
    유닛정보창 추가

--- Assets/02. Scripts/UI/UIMain.cs
+++ Assets/02. Scripts/UI/UIMain.cs
@@ -5,11 +5,12 @@ public class UIMain : UIBase
 {
     [SerializeField] private Button _startBtn;
     [SerializeField] private Button _settingBtn;
-    [SerializeField] private Button _unitStateBtn;
+    [SerializeField] private Button _heroesBtn;
 
     private void Start()
     {
         _startBtn.onClick.AddListener(() => { UIManager.Instance.OpenUI<UISelectStage>(); });
         _settingBtn.onClick.AddListener(() => { UIManager.Instance.OpenUI<UISetting>(); });
+        _heroesBtn.onClick.AddListener(() => {UIManager.Instance.OpenUI<>(); });
     }
 }
