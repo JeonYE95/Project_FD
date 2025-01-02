1a956fc96e44cd33fbf7a4450e183de4ad0bb363
GammaTT <alsdndl420@gmail.com>
2025-01-02 20:20:47 +0900

    UPDATE : BattleManager

--- Assets/02. Scripts/Managers/BattleManager.cs
+++ Assets/02. Scripts/Managers/BattleManager.cs
@@ -3,8 +3,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using UnityEditor.Experimental.GraphView;
-using UnityEditor.PackageManager;
 using UnityEngine;
 using static UnityEngine.GraphicsBuffer;
 using Random = UnityEngine.Random;
