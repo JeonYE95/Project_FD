17db7db4b8a2f824b5503a9464cda482e4670da1
moomin <amily8050@gmail.com>
2025-01-03 22:08:26 +0900

    UPDATE : 유닛 스프라이트 변경

--- Assets/02. Scripts/UI/UIGacha.cs
+++ Assets/02. Scripts/UI/UIGacha.cs
@@ -1,18 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
+using Art_Controller;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIGacha : UIBase
 {
+    [SerializeField] private TMP_Text _diamondTxt;
+
+    [SerializeField] private Button _gachaBtn1;
+    [SerializeField] private Button _gachaBtn10;
+
+    private PieceGacha pieceGacha = new PieceGacha();
+
     // Start is called before the first frame update
     void Start()
     {
         
+        _gachaBtn1.onClick.AddListener(() => { pieceGacha.PlayPieceGacha(); } );
+        _gachaBtn1.onClick.AddListener(() => { pieceGacha.PlayPieceGacha(); } );
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        _diamondTxt.text = GameManager.Instance.playerData.diamond.ToString();
     }
 }
