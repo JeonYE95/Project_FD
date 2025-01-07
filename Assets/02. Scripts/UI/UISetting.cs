e5c54fe59013998eb65febb2678f2ad1b4df401e
JeonYE95 <eunzse@gmail.com>
2025-01-07 12:35:23 +0900

    Add : Sound Option
    옵션창에서 사운드 세팅
    기기별로 사운드 저장

--- Assets/02. Scripts/UI/UISetting.cs
+++ Assets/02. Scripts/UI/UISetting.cs
@@ -4,9 +4,33 @@ using UnityEngine.UI;
 public class UISetting : UIBase
 {
     [SerializeField] private Button _exitBtn;
+    [SerializeField] private Slider _bgmSlider;
+    [SerializeField] private Slider _sfxSlider;
     
     void Start()
     {
+        if (_bgmSlider != null)
+        {
+            _bgmSlider.value = SoundManager.Instance.GetBGMVolume();
+            _bgmSlider.onValueChanged.AddListener(SetBGMVolume);
+        }
+
+        if (_sfxSlider != null)
+        {
+            _sfxSlider.value = SoundManager.Instance.GetSFXVolume();
+            _sfxSlider.onValueChanged.AddListener(SetSFXVolume);
+        }
+
         _exitBtn.onClick.AddListener(() => { Close(); });
+    }
+
+    private void SetBGMVolume(float value)
+    {
+        SoundManager.Instance.SetBGMVolume(value);
+    }
+
+    private void SetSFXVolume(float value)
+    {
+        SoundManager.Instance.SetSFXVolume(value);
     }
 }
